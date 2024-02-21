cd62c9cdfdd7153bdffbd578f98f98afa117ef1a
Godwill254 <simiyugodwill540@gmail.com>
2024-02-21 10:01:05 +0300

    1.2

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const search = () => {
     // Taking the value that is keyed-in the search box
-    const searchBox = document.getElementById("search-item").ariaValueMax.toUpperCase();
+    const searchBox = document.getElementById("search-item").value.toUpperCase();
     const storeItems = document.getElementById("product-list");
-    const product = document.querySelectorAll("product");
-    const pName = document.getElementsByTagName("h2");
-    
-    for(var i=0; i< pName.length; i++){
+    const product = document.querySelectorAll(".product");
+    const pName = storeItems.getElementsByTagName("h2");
+
+    for (var i = 0; i < pName.length; i++) {
         let match = product[i].getElementsByTagName('h2')[0];
 
-        if(match){
-            let textvalue = match.textContent || match.innerHTML
+        if (match) {
+            let textvalue = match.textContent || match.innerHTML;
 
-            if(textvalue.toUpperCase().indexOf(searchBox) > -1) {
-                product[i].style.display ="";
+            if (textvalue.toUpperCase().indexOf(searchBox) > -1) {
+                product[i].style.display = "";
             } else {
-                product[i].style.display ="none";
+                product[i].style.display = "none";
             }
         }
     }
